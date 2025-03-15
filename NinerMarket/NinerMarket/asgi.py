232d5cffdc61d35a14b9f2aaf74b15876477c438
NathanChen47 <xchen47@charlotte.edu>
2025-03-15 15:21:46 -0400

    File update
    updating similar files

--- NinerMarket/NinerMarket/asgi.py
+++ NinerMarket/NinerMarket/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for NinerMarket project.
+ASGI config for ninermarket project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NinerMarket.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ninermarket.settings')
 
 application = get_asgi_application()
