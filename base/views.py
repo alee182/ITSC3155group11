bbe53888aebca98b7816654de1a73d747e95c2ef
ann <alei1@charlotte.edu>
2025-04-01 13:50:48 -0400

    Merge branch 'main' into nav-bar

---
+++ base/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+# Create your views here.
+
+
+from django.shortcuts import render
+# Create your views here.
+def homePage(request):
+    return render(request, 'base/home.html')
+
+def explorePage(request):
+    return render(request, 'explore.html')
+
+def communityPage(request):
+    return render(request, 'community.html')
+
