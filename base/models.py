14ef3544a1451f0796c7eccbd39d7afe15ce79a9
s-luqman <sluqman@charlotte.edu>
2025-04-01 19:46:34 -0400

    adding more model info for messages/listings

--- base/models.py
+++ base/models.py
@@ -6,4 +6,15 @@ class Listing(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='static/images/listings', null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class DirectMessage(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
     
