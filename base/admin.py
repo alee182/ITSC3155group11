1a80b34171a7795c6ebdc14d8b2e3092ab21c856
s-luqman <sluqman@charlotte.edu>
2025-04-03 14:09:05 -0400

    custom user model (hopefully works)

--- base/admin.py
+++ base/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Listing, DirectMessage
+
+admin.site.register(Listing)
+admin.site.register(DirectMessage)
--- base/models.py
+++ base/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class Listing(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
@@ -9,6 +10,7 @@ class Listing(models.Model):
     image = models.ImageField(upload_to='static/images/listings', null=True, blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+    address = models.CharField(max_length=200, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -18,3 +20,11 @@ class DirectMessage(models.Model):
     recipient = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="recipient")
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    profile_pic = models.ImageField(upload_to='static/images/profile_pics', null=True, default='uncc-logo.png')
+
+
