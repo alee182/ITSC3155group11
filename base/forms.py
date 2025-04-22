a3aac5a3a6da04522195e178bf59a9ec57835038
NathanChen47 <xchen47@charlotte.edu>
2025-04-22 14:18:21 -0400

    Keep these changes

--- base/forms.py
+++ base/forms.py
@@ -1,7 +1,28 @@
-from django.forms import ModelForm
-from .models import Listing
+from django import forms
+from .models import Comment
+from base.models import User
 
-class ListingForm(ModelForm):
+class CommentForm(forms.ModelForm):
     class Meta:
-        model = Listing
-        fields = '__all__'
+        model = Comment
+        fields = ['body']
+        widgets = {
+            'body': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Write a comment...'}),
+        }
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['email', 'first_name', 'last_name', 'profile_pic']
+        widgets = {
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'profile_pic': forms.FileInput(attrs={'class': 'form-control'}),
+        }
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if not email.lower().endswith('@charlotte.edu'):
+            raise forms.ValidationError('Email must end with "@charlotte.edu"')
+        return email
